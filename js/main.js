caebae5a9d200be15f100ed6483a28551dee279e
Dave Bilotta <davebilotta@gmail.com>
2015-03-28 20:22:33 -0400

    Add images for score counter, new images

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ var ships;
 var obstacles;
 var scoreText;
 var score;
+var numShips;
 
 var width = 1024;
 var height = 768;
 
+// Ship/Obstacle speeds
 var SHIP1_SPEED = 40;
 var SHIP2_SPEED = 60;
 var UFO_SPEED = 20;
@@ -18,7 +20,10 @@ var gameOver = false;
 
 var RADIANS_TO_DEGREES = 57.2957795;
 
-var timer;
+var scoreTextA, scoreTextB, scoreTextC;
+// offset of UI elements
+var horizontalOffset = 20;
+var verticalOffset = 15;
 
 var game = new Phaser.Game(width, height, Phaser.AUTO, '', 
   { preload: preload, 
@@ -34,9 +39,22 @@ function preload() {
       game.load.image('meteorSmall','images/meteorSmall.png');
       game.load.image('meteorLarge','images/meteorBig.png');
 
-      game.load.image('background', 'images/starBackground.png');
+      game.load.image('background', 'images/bg/black.png');
+      //game.load.image('background', 'images/bg/blue.png');
+      //game.load.image('background', 'images/bg/darkPurple.png');
+      //game.load.image('background', 'images/bg/purple.png');
 
-    
+      game.load.image('0', 'images/numbers/0.png');
+      game.load.image('1', 'images/numbers/1.png');
+      game.load.image('2', 'images/numbers/2.png');
+      game.load.image('3', 'images/numbers/3.png');
+      game.load.image('4', 'images/numbers/4.png');
+      game.load.image('5', 'images/numbers/5.png');
+      game.load.image('6', 'images/numbers/6.png');
+      game.load.image('7', 'images/numbers/7.png');
+      game.load.image('8', 'images/numbers/8.png');
+      game.load.image('9', 'images/numbers/9.png');
+   
 
   }
 
@@ -47,11 +65,22 @@ function create() {
       obstacles = game.add.group();
 
       score = 0;
+      numShips = 0;
+
       scoreText = game.add.text(0, 0, ("Score: " + score), {      
         font: "30px Arial", 
         fill: "#ff0044", 
         align: "center" 
       });
+
+      // score is 3 digits (2a, 2b, 2c left to right)
+      // number images are 19x19
+      var w = 20;
+      scoreTextA = game.add.image(width - (horizontalOffset + (w * 3)),verticalOffset,'0');
+      scoreTextB = game.add.image(width - (horizontalOffset + (w * 2)),verticalOffset,'0');
+      scoreTextC = game.add.image(width - (horizontalOffset + w),verticalOffset,'0');
+
+      console.log('0'.height);
 
       // game.input.onDown.add(newShip, this);
 
@@ -72,11 +101,13 @@ function create() {
   function spawn() {
      //spawnDelta;
      //console.log(timer.expired);
-     
+     if (!gameOver) {
      newShip();
 
      // TODO: Maybe create new obstacle?
-  }
+
+   }
+  } // end spawn function
 
 function randNum(max) {
   return game.rnd.integerInRange(0,max);
@@ -128,8 +159,8 @@ function newShip() {
     speed);
   // returns result in radians
 
-  console.log("Rotation is " + rot);
-  console.log("DEGREES is " +rot * RADIANS_TO_DEGREES);
+  numShips++;
+  console.log("Ship created: total count is: " + numShips);
   //if (rot < 0) { 
    // ship.angle = rot * RADIANS_TO_DEGREES + 90;
   //}
@@ -211,6 +242,35 @@ function checkLanding() {
 } // end checkLanding
 
 function updateUI() {  
-  scoreText.setText("Score: " + score);
+  // TODO: Remove this once testing is done 
+  var s = numShips;
+
+  // 
+  if (s > 999) s = 999;
+  var padScore = padNum(s) + "";
+  console.log("score = " + padScore)
+  
+  var a = padScore.substring(0,1);
+  var b = padScore.substring(1,2);
+  var c = padScore.substring(2,3);
+
+  scoreTextA.loadTexture(a);
+  scoreTextB.loadTexture(b);
+  scoreTextC.loadTexture(c);
+
 }
 
+function padNum(num) {
+  // Pads score 
+  // TODO: Make this better
+  if (num < 10) {
+     return "00" + num;
+  }
+  else {
+    if (num < 100) {
+      return ("0") + num;
+    }
+    else return num;
+  }
+
+}
